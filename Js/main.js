062d064ffcb2c3d7b81384a703e5389174f7629a
Thomas <genet.thomas62@gmail.com>
2020-01-24 16:33:46 +0100

    Commentaires ajouter et beautify

--- Js/main.js
+++ Js/main.js
@@ -1,50 +1,51 @@
-let monSlider = new Slider ("diapo");
+//Nouvel instance diapo
+let monSlider = new Slider("diapo");
 
 let pauseButton = document.getElementById('pause');
 
 let btnPrev = document.getElementById('precedent');
 let btnNext = document.getElementById('suivant');
 
+//Affichage du diaporama
 monSlider.affichage(monSlider.diaporama);
 
 pauseButton.innerHTML = 'Pause';
-		monSlider.playing = true;
-		monSlider.slideInterval = setInterval(function(){
-		monSlider.affichage (monSlider.diaporama + 1);
-	}
-	,5000);
+monSlider.playing = true;
+monSlider.slideInterval = setInterval(function () {
+	monSlider.affichage(monSlider.diaporama + 1);
+}, 5000);
 
-pauseButton.onclick = function(){
-	if(monSlider.playing){
+pauseButton.onclick = function () {
+	if (monSlider.playing) {
 		pauseButton.innerHTML = 'Play';
 		monSlider.playing = false;
 		clearInterval(monSlider.slideInterval);
-}
-	else{ pauseButton.innerHTML = 'Pause';
+	} else {
+		pauseButton.innerHTML = 'Pause';
 		monSlider.playing = true;
-		monSlider.slideInterval = setInterval(function(){
-		monSlider.affichage (monSlider.diaporama + 1);
+		monSlider.slideInterval = setInterval(function () {
+			monSlider.affichage(monSlider.diaporama + 1);
+		}, 5000);
 	}
-	,5000);
-}
 };
 
-btnNext.onclick = function (){
-	monSlider.affichage (monSlider.diaporama + 1);
+//Flèche afficher gauche & droite
+btnNext.onclick = function () {
+	monSlider.affichage(monSlider.diaporama + 1);
 }
-btnPrev.onclick = function (){
-	monSlider.affichage (monSlider.diaporama - 1);
+btnPrev.onclick = function () {
+	monSlider.affichage(monSlider.diaporama - 1);
 }
-
-document.onkeydown = function(e) {
+//Flèche clavier gauche & droite 
+document.onkeydown = function (e) {
 	switch (e.keyCode) {
 		case 37:
-		   //left
-		   monSlider.affichage (monSlider.diaporama - 1);
-		   break;
+			//left
+			monSlider.affichage(monSlider.diaporama - 1);
+			break;
 		case 39:
-		   //right
-		   monSlider.affichage (monSlider.diaporama + 1);
-		   break;          
-   }
+			//right
+			monSlider.affichage(monSlider.diaporama + 1);
+			break;
+	}
 }
